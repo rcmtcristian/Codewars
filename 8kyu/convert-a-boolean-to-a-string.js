a67291e2bedc9596a695240f1688237231500dfe
rcmtcristian <rcmtcristian@gmail.com>
2022-08-25 15:42:53 -0400

    new code war

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+//   Note: Only valid inputs will be given.
+
+//     STRINGSFUNDAMENTALS
+
+//My solution 
+function booleanToString(b) {
+  return b.toString()
+}
+
+//other solutions 
+function booleanToString(b) {
+  return String(b);
+}
+
+//
+function booleanToString(b) {
+  return b ? 'true' : 'false';
+}
+574 similar co
