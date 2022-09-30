c104316e5ac9f65e4b197679bf1420c77af19621
rcmtcristian <rcmtcristian@gmail.com>
2022-09-30 12:32:54 -0300

    feat:contamination #1-string 8kyu

---
+++ 8kyu/contamination-1-string.js
@@ -0,0 +1,43 @@
+// DESCRIPTION:
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+// STRINGSFUNDAMENTALS
+
+// my solution
+function contamination(text, char) {
+  return char.repeat(text.length);
+}
+
+// other
+function contamination(text, char) {
+  return char.repeat(text.length);
+}
+
+//
+
+function contamination(text, char) {
+  return text.split("").fill(char).join("");
+}
+
+/////////////////////////////////////////////////
+
+function contamination(text, char) {
+  return char.repeat(text.length);
+}
+
+/////////////////////////////////////////////////
+
+function contamination(text, char) {
+  return text.replace(/./g, char);
+}
