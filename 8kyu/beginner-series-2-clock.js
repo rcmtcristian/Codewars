a5eec79acff52a7e3abe48244cabf3b34d8aff9b
Cris <rcmtcristian@gmail.com>
2022-10-15 12:47:33 -0400

    feat: beginner series 2 clock 8kyu

---
+++ 8kyu/beginner-series-2-clock.js
@@ -0,0 +1,32 @@
+// DESCRIPTION:
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+// FUNDAMENTALS
+
+// my solution 
+function past(h, m, s){
+    //#Happy Coding! ^_^
+    
+    return new Date().setHours(h,m,s) - new Date().setHours(0,0,0)
+  }
+
+  // 
+  const past = (h,m,s) => 1000 * (3600 * h + 60 * m + s);
+
+  //
+  function past(h, m, s){
+    return ((h*3600)+(m*60)+s)*1000;
+  }
