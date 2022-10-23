8d0ee29d8348b09540f5704014619c7dcee65992
rcmtcristian <rcmtcristian@gmail.com>
2022-10-23 12:33:41 -0400

    feat: five without numbers 8kyu

---
+++ 8kyu/five-without-numbers.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// RESTRICTEDFUNDAMENTALSPUZZLES
+
+// My solution
+function unusualFive() {
+  let it = "fives";
+
+  return it.length;
+}
+
+//
+function unusualFive() {
+  return "jesus".length;
+}
+//
+const unusualFive = () => Math.hypot(Math.ceil(Math.PI), Math.floor(Math.PI));
