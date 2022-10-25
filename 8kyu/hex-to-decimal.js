70f9e9bbfa05e9166ac016c0e5d2e68b0cf9510a
rcmtcristian <rcmtcristian@gmail.com>
2022-10-25 10:54:27 -0400

    feat: hex to decimal 8kyu

---
+++ 8kyu/hex-to-decimal.js
@@ -0,0 +1,41 @@
+// DESCRIPTION:
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// FUNDAMENTALS
+
+// my solution
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
+
+//
+const hexToDec = (h) => parseInt(h, 16);
+
+//
+function hexToDec(hexString) {
+  let hexVal = {
+      a: 10,
+      b: 11,
+      c: 12,
+      d: 13,
+      e: 14,
+      f: 15,
+    },
+    clone = hexString.split("").map((x) => x.toLowerCase()),
+    isNeg = false;
+  if (clone[0] === "-") {
+    isNeg = true;
+    clone.splice(0, 1);
+  }
+  let output = Number(
+    clone
+      .reverse()
+      .map((e, i) =>
+        hexVal[e] ? hexVal[e] * Math.pow(16, i) : e * Math.pow(16, i)
+      )
+      .reduce((a, b) => a + b),
+    10
+  );
+  return isNeg ? output * -1 : output;
+}
