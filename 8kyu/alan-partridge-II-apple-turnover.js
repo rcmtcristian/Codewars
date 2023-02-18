40e9041fa11a72b5d9285e6ab3b0b7525d3da993
rcmtcristian <rcmtcristian@gmail.com>
2023-02-17 19:01:07 -0500

    feat: alan partridge II apple turnover 8ky

---
+++ 8kyu/alan-partridge-II-apple-turnover.js
@@ -0,0 +1,21 @@
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Note: Input will either be a positive integer (or a string for untyped languages).
+
+// Other katas in this series:
+// Alan Partridge I - Partridge Watch
+
+// Main solution:
+function apple(x) {
+  return x ** 2 > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
+
+// Other Solution
+function apple(x) {
+  return Math.pow(x, 2) > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
