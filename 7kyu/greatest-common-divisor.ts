12ed97f580aeb2593f20e122388346f608a7fe34
rcmtcristian <rcmtcristian@gmail.com>
2023-05-07 21:52:45 -0400

    :sparkles: greatest common divisor 7kyu

---
+++ 7kyu/greatest-common-divisor.ts
@@ -0,0 +1,24 @@
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+// Main Solution
+function mygcd(x,y){
+  if (y === 0) {
+    return x;
+  }
+   return mygcd(y, x % y);
+}
+
+// Refactored Solution
+function mygcd(x: number, y: number): number {
+  if (y === 0) {
+    return x;
+  }
+  return mygcd(y, x % y);
+}
+
+// Other Solutions
+function mygcd(x,y){
+  return y == 0 ? x : mygcd(y, x % y)
+}
