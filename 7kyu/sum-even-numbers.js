7794ff15aee57d449cc05cd4e1c06d4bb57637e6
rcmtcristian <rcmtcristian@gmail.com>
2023-06-17 18:58:32 -0400

    ✨ Sum even numbers - 7kyu

---
+++ 7kyu/sum-even-numbers.js
@@ -0,0 +1,23 @@
+// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+// Only numbers without decimals like 4 or 4.0 can be even.
+
+// The input is a sequence of numbers: integers and/or floats.
+
+// Examples
+// [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+// []                               -->  0
+
+// Main Solution
+function sumEvenNumbers(input) {
+  return input.reduce((sum, num) => {
+    if (num % 2 === 0) {
+      return sum + num;
+    } else {
+      return sum;
+    }
+  }, 0);
+}
+
+// Alternative Solution
+sumEvenNumbers = input => input.filter(x => x % 2 == 0).reduce((x, y) => x + y, 0)
